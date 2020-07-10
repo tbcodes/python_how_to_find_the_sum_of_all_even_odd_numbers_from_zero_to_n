9426fabb0e43395ffb1f03abdde9000d86c79258
TruzzBlogg <soypacos@hotmail.com>
2020-07-10 16:12:49 +0200

    Source code updated

--- 16-Find_sum_of_even_odd_numbers.py
+++ 16-Find_sum_of_even_odd_numbers.py
@@ -1,6 +1,26 @@
 # How to calculate the sum of all even/odd numbers from zero to 'n'
 # Youtube URL: https://youtu.be/xcu-1cO014Q
 
+# **********************************************************************
+# Sum all even numbers from zero to 'n'
+
+total = 0
+# num = 500
+num = int(input("Please, enter a number: "))
+even_numbers = []
+
+for i in range(0, num + 1):
+  if i % 2 == 0:
+    even_numbers.append(i)
+    total += i
+
+print(f"All even numbers from 0 to {num} is: {even_numbers}")
+print("········································")
+print("The sum of even numbers from 0 to {} is: {}".format(num, total))
+
+# **********************************************************************
+# Sum all odd numbers from zero to 'n'
+
 total = 0
 # num = 500
 num = int(input("Please, enter a number: "))
